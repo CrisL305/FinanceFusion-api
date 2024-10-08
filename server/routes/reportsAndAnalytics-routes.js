3eafa263fdecfd91ba211e8d35c1b8717d35f2dc
Cristopher Lopez <163314523+CrisL305@users.noreply.github.com>
2024-10-07 21:37:56 -0400

    Revert "gitOAuth"

--- server/routes/reportsAndAnalytics-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-//Retrieves data for the user and creates report charts and analyses the best course of action.
-router.get("/", (_req, res) => {
-    //This implemantation is to make sure the endpoint GET works
-    res.send("You have reached the reports and analytics GET List");
-});
-
-module.exports = router;
